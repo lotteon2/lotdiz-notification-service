2c2a663be1f1257fbd6f39114a278fcdad39b026
CokeLee777 <akak640@kyonggi.ac.kr>
2023-10-24 23:56:50 +0900

    feat: add crossorigin annotation

--- src/main/java/com/lotdiz/notificationservice/controller/restcontroller/NotificationRestController.java
+++ src/main/java/com/lotdiz/notificationservice/controller/restcontroller/NotificationRestController.java
@@ -15,11 +15,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}, allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
